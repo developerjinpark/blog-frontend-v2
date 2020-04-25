8a7de4ebf5888bd920fd3cc3679e718c9fefceab
Jin <jinpark1504@gmail.com>
2020-04-24 20:03:01 -0400

    project structure

---
+++ src/store/modules/index.js
@@ -0,0 +1,7 @@
+export { default as editer } from './editor'
+export { default as base } from './base'
+export { default as list } from './list'
+export { default as post } from './post'
+
+// manage async actions
+export { penderReducer as pender } from 'redux-pender'
