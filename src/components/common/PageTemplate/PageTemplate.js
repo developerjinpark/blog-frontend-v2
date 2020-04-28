a069eb145957ad69e3a17dba47e3b9d4da871fb3
Jin <jinpark1504@gmail.com>
2020-04-28 00:50:49 -0400

    initial UI

--- src/components/common/PageTemplate/PageTemplate.js
+++ src/components/common/PageTemplate/PageTemplate.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import classNames from 'classnames/bind'
 import styles from './PageTemplate.scss'
+import Header from 'components/common/Header'
+import Footer from 'components/common/Footer'
 
 const cx = classNames.bind(styles)
 
-const PageTemplate = () => (
+const PageTemplate = ({children}) => (
     <div className={cx('page-template')}>
-    PageTemplate
+        <Header />
+        <main>{children}</main>
+        <Footer />
     </div>
 )
 
